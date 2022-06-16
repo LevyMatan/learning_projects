4c187a1d45a169d186e16853661c0acf4384ed24
Matan Levy <levymatanlevy@gmail.com>
2022-06-16 17:53:59 +0300

    Update tmp.cpp

--- src/tmp.cpp
+++ src/tmp.cpp
@@ -1,3 +1,15 @@
 #include "project/tmp.hpp"
 
 int tmp::add(int a, int b) { return a + b; }
+int tmp::matan(int hello)
+{
+  switch(hello)
+  {
+    case 1:
+      return 2;
+    case 2:
+      return 100;
+    default:
+      return 0;
+  }
+}
